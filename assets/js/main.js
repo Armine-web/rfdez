b6aa65237928aeb35a36457c9255d5c8cd7d3812
Armine Aghajanyan <armine.aghajanyan123@gmail.com>
2025-10-28 02:16:39 +0400

    new commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,34 @@ function updateStatus() {
   
   window.addEventListener('online', updateStatus);
   window.addEventListener('offline', updateStatus);
+
+  ///burger menu
+
+const burger = document.getElementById('burger');
+const burgerIcon = document.getElementById('burgerIcon');
+
+const staticMenu = document.getElementById('menuWindow');
+const popupMenu = staticMenu.cloneNode(true);
+popupMenu.classList.add('popup');
+document.body.appendChild(popupMenu);
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  popupMenu.classList.toggle('active');
+
+  if (burger.classList.contains('active')) {
+    burgerIcon.src = "assets/images/ui/close.png";
+  } else {
+    burgerIcon.src = "assets/images/ui/burger-menu.png";
+  }
+});
+
+
+//more services
+
+document.querySelectorAll('.service-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    this.parentElement.classList.toggle('active');
+  });
+});
