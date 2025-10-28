4dde9b1f19744dc20509d5d0c9ecf68d5a11a764
Armine Aghajanyan <armine.aghajanyan123@gmail.com>
2025-10-29 02:15:45 +0400

    quiz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 //updatae ofline vs online
 function updateStatus() {
-    const statusIndicator = document.getElementById('status-indicator');
-  
-    if (navigator.onLine) {
-      statusIndicator.classList.remove('offline');
-      statusIndicator.classList.add('online');
-    } else {
-      statusIndicator.classList.remove('online');
-      statusIndicator.classList.add('offline');
-    }
-  }
-  
-  updateStatus();
-  
-  window.addEventListener('online', updateStatus);
-  window.addEventListener('offline', updateStatus);
+  const statusIndicator = document.getElementById('status-indicator');
 
-  ///burger menu
+  if (navigator.onLine) {
+    statusIndicator.classList.remove('offline');
+    statusIndicator.classList.add('online');
+  } else {
+    statusIndicator.classList.remove('online');
+    statusIndicator.classList.add('offline');
+  }
+}
+
+updateStatus();
+
+window.addEventListener('online', updateStatus);
+window.addEventListener('offline', updateStatus);
+
+///burger menu
 
 const burger = document.getElementById('burger');
 const burgerIcon = document.getElementById('burgerIcon');
@@ -46,3 +46,39 @@ document.querySelectorAll('.service-link').forEach(link => {
     this.parentElement.classList.toggle('active');
   });
 });
+
+
+//towns-slider
+const track = document.querySelector('.quiz-town-connetn');
+const prev = document.querySelector('.arrow-left');
+const next = document.querySelector('.quiz-arrow-right');
+
+const totalItems = track.children.length;
+const visibleItems = 8; 
+const itemsPerRow = 4;
+let currentIndex = 0;
+
+function updateSlider() {
+
+  const rowsToMove = Math.floor(currentIndex / itemsPerRow);
+  const itemHeight = track.children[0].offsetHeight + 10; 
+  const rowOffset = rowsToMove * itemHeight;
+  track.style.transform = `translateY(-${rowOffset}px)`;
+}
+
+prev.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex -= itemsPerRow; 
+    if (currentIndex < 0) currentIndex = 0;
+    updateSlider();
+  }
+});
+
+next.addEventListener('click', () => {
+  if (currentIndex < totalItems - visibleItems) {
+    currentIndex += itemsPerRow; 
+    updateSlider();
+  }
+});
+
+
